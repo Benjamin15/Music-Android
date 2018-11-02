8bc588ba89010f8253ed20027e70fce30ee22e3d
smouss <soukainamoussaoui1@gmail.com>
2018-11-02 18:03:41 -0400

    ANDROID - SM: création bouton et service pour changer le mdp

--- ApplicationCafeBistro/app/src/main/java/applicationcafebistro/ca/polymtl/equipe3/client/applicationcafebistro/components/ChangePasswordButton.java
+++ ApplicationCafeBistro/app/src/main/java/applicationcafebistro/ca/polymtl/equipe3/client/applicationcafebistro/components/ChangePasswordButton.java
@@ -2,38 +2,50 @@ package applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.co
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 
 import org.json.JSONException;
+import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.service.ChangePasswordService;
+import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.view.Home;
 
-import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.service.MusicDetailsService;
-import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.view.MusicDetails;
 
 public class ChangePasswordButton extends android.support.v7.widget.AppCompatButton implements View.OnClickListener {
     private Context context;
-    private Bundle fragmentBundle;
-    private String id; //to be changed by the id from the http request
-
-    private MusicDetailsService musicDetailsService;
+    private ChangePasswordService changePasswordService;
 
     public ChangePasswordButton(Context context){
         super(context);
-        this.context=context;
+        this.context = context;
+        this.changePasswordService = new ChangePasswordService(context);
+        init();
+    }
+
+    public ChangePasswordButton(Context context, AttributeSet attrs) {
+        super(context, attrs, android.support.v7.appcompat.R.attr.buttonStyle);
+        this.changePasswordService= new ChangePasswordService(context);
+        this.context = context;
+        init();
+    }
+
+    public ChangePasswordButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.changePasswordService = new ChangePasswordService(context);
+        this.context = context;
+        init();
     }
 
     private void init() {
         setOnClickListener(this);
     }
-
     @Override
     public void onClick(View view){
         try{
-            musicDetailsService.musicDetails(id);
+            changePasswordService.changePassword();
         }catch (JSONException e){
             e.printStackTrace();
         }
-
-        Intent intent=new Intent (this.context, MusicDetails.class);
+        Intent intent = new Intent(this.context, Home.class);
         context.startActivity(intent);
     }
 
