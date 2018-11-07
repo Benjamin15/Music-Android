9850a4cffe9a45d6de1bab3826ab97e84fe44934
benjamin15 <benjamin.heinen@polytml.ca>
2018-11-06 20:06:35 -0500

    async get rest de la liste à chaque seconde

--- ApplicationCafeBistro/app/src/main/java/applicationcafebistro/ca/polymtl/equipe3/client/applicationcafebistro/view/Home.java
+++ ApplicationCafeBistro/app/src/main/java/applicationcafebistro/ca/polymtl/equipe3/client/applicationcafebistro/view/Home.java
@@ -1,36 +1,26 @@
 package applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.view;
 
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.R;
 import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.components.ListMusicForUser;
-import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.service.CommunicationRest;
+import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.service.ServiceGetList;
 
 public class Home extends AppCompatActivity {
 
-    private ListMusicForUser listMusicUser;
+    public static ListMusicForUser listMusicUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
-        //IdentificationService identificationService =
-        //      new IdentificationService(getApplicationContext());
+        Intent intent = new Intent(Home.this, ServiceGetList.class);
+        intent.putExtra("listMusic", (Parcelable) listMusicUser);
+        startService(intent);
 
         listMusicUser = findViewById(R.id.list_music);
-        CommunicationRest communication = new CommunicationRest(
-                getResources().getString(R.string.list_music_test) + "1",
-                "GET",
-                this.getApplicationContext(),
-                listMusicUser);
-        try {
-            communication.send((JSONObject) null);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 }
