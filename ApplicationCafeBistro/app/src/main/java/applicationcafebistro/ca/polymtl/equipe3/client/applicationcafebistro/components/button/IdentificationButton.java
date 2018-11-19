af12aa7bf9e6cb493aed6e0a5dd85217edb2a76a
Wassim Guellati <MW-Guellati@outlook.fr>
2018-11-18 23:37:39 -0500

    Interface Login terminé (Client et Admin)

--- ApplicationCafeBistro/app/src/main/java/applicationcafebistro/ca/polymtl/equipe3/client/applicationcafebistro/components/button/IdentificationButton.java
+++ ApplicationCafeBistro/app/src/main/java/applicationcafebistro/ca/polymtl/equipe3/client/applicationcafebistro/components/button/IdentificationButton.java
@@ -1,11 +1,14 @@
 package applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.components.button;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.widget.AppCompatButton;
 import android.text.Editable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.Window;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,6 +31,7 @@ public class IdentificationButton extends AppCompatButton
         implements View.OnClickListener, ComponentsListener {
 
     private String login;
+    private Dialog dialog;
 
     public IdentificationButton(Context context) {
         super(context);
@@ -46,30 +50,39 @@ public class IdentificationButton extends AppCompatButton
 
     private void init() {
         setOnClickListener(this);
+        dialog = new Dialog(getContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.setContentView(R.layout.login_admin_dialog);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.AppearDialogAnimation;
     }
 
     @Override
     public void onClick(View view) {
-        try {
-            final String GET = "GET";
-            String macAddress = DeviceInformation.getMACAddress("wlan0");
-            String ipv4 = DeviceInformation.getIPAddress(true);
-            HashMap map = new HashMap();
-            System.out.println("MAC :" + macAddress);
-            map.put(getContext().getString(R.string.ip), ipv4);
-            map.put(getContext().getString(R.string.mac), macAddress);
-            map.put(getContext().getString(R.string.login_param), login);
-            JSONObject body = new JSONObject(map);
+        if(!login .equals(getContext().getString(R.string.admin_username))) {
+            try {
+                final String GET = "GET";
+                String macAddress = DeviceInformation.getMACAddress("eth0");
+                String ipv4 = DeviceInformation.getIPAddress(true);
+                HashMap map = new HashMap();
+                System.out.println("MAC :" + macAddress);
+                map.put(getContext().getString(R.string.ip), ipv4);
+                map.put(getContext().getString(R.string.mac), macAddress);
+                map.put(getContext().getString(R.string.login_param), login);
+                JSONObject body = new JSONObject(map);
 
-            String urlParameter = URLEncoder.encode(body.toString(), getContext().getString((R.string.utf8)));
-            CommunicationRest communication = new CommunicationRest(
-                    getResources().getString(R.string.identification) + getContext().getString(R.string.body) + urlParameter,
-                    GET,
-                    view,
-                    this);
-            communication.send();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+                String urlParameter = URLEncoder.encode(body.toString(), getContext().getString((R.string.utf8)));
+                CommunicationRest communication = new CommunicationRest(
+                        getResources().getString(R.string.identification) + getContext().getString(R.string.body) + urlParameter,
+                        GET,
+                        view,
+                        this);
+                communication.send();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }else{
+            dialog.show();
         }
     }
 
