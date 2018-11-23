d651dcb7a59a12fa9de7429f58490f208f3d79a5
Wassim Guellati <MW-Guellati@outlook.fr>
2018-11-22 19:46:37 -0500

    Fragments listuser, blacklist avec accés depuis navView

---
+++ ApplicationCafeBistro/app/src/main/java/applicationcafebistro/ca/polymtl/equipe3/client/applicationcafebistro/view/ListMusic/Admin/FragmentBlackList.java
@@ -0,0 +1,42 @@
+package applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.view.ListMusic.Admin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.R;
+import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.components.snackbar.SnackBarSuccess;
+import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.service.ServiceGetList;
+
+public class FragmentBlackList extends Fragment {
+
+    private View view;
+    public FragmentBlackList(){
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    private void initSnackBar(){
+        Intent intent = new Intent(getActivity(), ServiceGetList.class);
+        SnackBarSuccess.make(view, getContext(), intent.getStringExtra(getString(R.string.welcome_message_key)), 3000);
+        SnackBarSuccess.show();
+    }
+    private void initRecyclerView(){
+
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.blacklist_fragment,container,false);
+        initRecyclerView();
+      //  initSnackBar();
+        return view;
+    }
+}
