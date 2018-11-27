84f55559c56267cd2540a0c00ff07a649207bb5a
Wassim Guellati <MW-Guellati@outlook.fr>
2018-11-26 23:14:04 -0500

    statistics

--- ApplicationCafeBistro/app/src/main/java/applicationcafebistro/ca/polymtl/equipe3/client/applicationcafebistro/view/ListMusic/Admin/FragmentStatistics.java
+++ ApplicationCafeBistro/app/src/main/java/applicationcafebistro/ca/polymtl/equipe3/client/applicationcafebistro/view/ListMusic/Admin/FragmentStatistics.java
@@ -6,12 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.R;
 import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.communication.CommunicationRest;
+import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.components.ComponentsListener;
+import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.view.ListMusic.FragmentCommonList;
+import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.view.ListMusic.FragmentPersonalList;
+import applicationcafebistro.ca.polymtl.equipe3.client.applicationcafebistro.view.ListMusic.ListMusic;
 
-public class FragmentStatistics extends Fragment {
+public class FragmentStatistics extends Fragment implements ComponentsListener {
     private View view;
+    private TextView postedSongs, deletedSongs, averageTime, users;
 
     public FragmentStatistics() {
 
@@ -25,7 +34,35 @@ public class FragmentStatistics extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.blacklist_fragment, container, false);
+        view = inflater.inflate(R.layout.statistics_fragment, container, false);
+        postedSongs =  view.findViewById(R.id.posted_songs);
+        averageTime =  view.findViewById(R.id.average_time);
+        deletedSongs =  view.findViewById(R.id.deleted_songs);
+        users = view.findViewById(R.id.distinct_users);
+        getStatistics();
         return view;
+    }
+
+    public void getStatistics(){
+        final String GET = "GET";
+        CommunicationRest communication = new CommunicationRest(
+                getContext().getString(R.string.statistics),
+                GET,
+                this.view,
+                this
+        );
+        communication.send(null);
+    }
+
+    @Override
+    public void update(JSONObject json) {
+        try {
+            postedSongs.setText(json.getInt(getContext().getString(R.string.statistics_songs)));
+            averageTime.setText(json.getString(getContext().getString(R.string.statistics_average_time)));
+            deletedSongs.setText(json.getInt(getContext().getString(R.string.statistics_deleted_songs)));
+            users.setText(json.getInt(getContext().getString(R.string.statistics_users)));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
